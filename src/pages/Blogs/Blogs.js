a6ee29dcea18acded94ea0c2a964e82794f9b6c7
sohailul <sohailulalam@gmail.com>
2022-05-23 16:13:00 +0600

    Blogs and My portfolio component designed

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -2,8 +2,60 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is Blog page</h1>
+        <div className='min-h-screen mt-12 px-5 py-5'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center'>
+                <div>
+                    <p className='text-xl font-bold'>How will you improve the performance of a React Application?</p>
+                    <p>
+                        <ul>
+                            <li>1. Keeping component state local where necessary.</li>
+                            <li>2. Memoizing React components to prevent unnecessary re-renders.</li>
+                            <li>3. Code-splitting in React using dynamic import().</li>
+                            <li>4. Windowing or list virtualization in React applications</li>
+                            <li>5. Lazy loading images in React</li>
+                        </ul>
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'>What are the different ways to manage a state in a React application?</p>
+                    <p>
+                        The Four Kinds of React State to Manage
+                        When we talk about state in our applications, it’s important to be clear about what types of state actually matter.
+                        There are four main types of state you need to properly manage in your React apps:
+                        <ul>
+                            <li>1. Local state</li>
+                            <li>2. Global state</li>
+                            <li>3. Server state</li>
+                            <li>4. URL state</li>
+                        </ul>
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'> How does prototypical inheritance work?</p>
+                    <p>
+                        Every object with its methods and properties contains an internal and hidden property known as [[Prototype]]. The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'> What is a unit test? Why should write unit tests?</p>
+                    <p>
+                        Unit testing involves the testing of each unit or an individual component of the software application. It is the first level of functional testing. The aim behind unit testing is to validate unit components with its performance.
+                        A unit is a single testable part of a software system and tested during the development phase of the application software.
+
+                        Unit testing allows software developers to actually think through the design of the software and what has to be done before they write the code. This can help them to stay focused and can also help them to create much better designs. Testing a code early on can help to define what that piece of code is really responsible for.
+                    </p>
+                </div>
+                <div>
+                    <p className='text-xl font-bold'> Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</p>
+                    <p>
+                        <ul>
+                            <li>1. If you update it directly, calling the setState() afterward may just replace the update you made.</li>
+                            <li>2. When you directly update the state, it does not change this.state immediately. Instead, it creates a pending state transition, and accessing it after calling this method will only return the present value.</li>
+                            <li>3. You will lose control of the state across all components.</li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { AiOutlineMail, AiFillGithub, AiFillEye } from 'react-icons/ai';
+import { FaUserGraduate } from 'react-icons/fa';
 
 const Portfolio = () => {
     return (
-        <div>
-            <h1>My portfolio</h1>
+        <div className='mt-12 mb-12'>
+            <div class="card card-side bg-base-100 shadow-xl mb-24">
+                <div class="card-body">
+                    <h2 class="text-5xl font-bold">Sohailul Alam</h2>
+                    <p className='text-3xl'>Web Developer</p>
+                    <p className='text-xl lg:text-2xl mt-5 flex items-center'><AiOutlineMail />&nbsp; Sohailulalam@gmail.com</p>
+                    <p className='text-xl lg:text-2xl flex items-center'><FaUserGraduate />&nbsp; B.Sc(Hon's) in Computer Science & Engineering</p>
+                    <h2 className='mt-12 mb-5 text-4xl font-bold'>Skills</h2>
+                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 justify-center'>
+                        <button class="btn btn-sm btn-primary">JavaScript(ES6)</button>
+                        <button class="btn btn-sm btn-primary">React JS</button>
+                        <button class="btn btn-sm btn-primary">Node JS</button>
+                        <button class="btn btn-sm btn-primary">Express JS</button>
+                        <button class="btn btn-sm btn-primary">MongoDB</button>
+                        <button class="btn btn-sm btn-primary">Firebase</button>
+                        <button class="btn btn-sm btn-primary">React Hooks</button>
+                        <button class="btn btn-sm btn-primary">HTML5</button>
+                        <button class="btn btn-sm btn-primary">CSS3</button>
+                        <button class="btn btn-sm btn-primary">Bootstrap</button>
+                        <button class="btn btn-sm btn-primary">Tailwind CSS</button>
+                        <button class="btn btn-sm btn-primary">Daisy UI</button>
+                        <button class="btn btn-sm btn-primary">Laravel</button>
+                        <button class="btn btn-sm btn-primary">WordPress</button>
+                        <button class="btn btn-sm btn-primary">MySQL</button>
+                    </div>
+                </div>
+            </div>
+            <h1 className='text-4xl font-bold text-center mb-12 divider'><span className='text-primary'>Projects</span></h1>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center'>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://live.staticflickr.com/65535/52093303459_eb128e5969_b.jpg" alt="" /></figure>
+                    <div class="card-body bg-secondary text-white">
+                        <h2 className="text-3xl font-bold text-center mb-2">Coders Shopping</h2>
+                        <div class="card-actions">
+                            <div class="badge badge-outline">React</div>
+                            <div class="badge badge-outline"> Node Js</div>
+                            <div class="badge badge-outline"> Express JS</div>
+                            <div class="badge badge-outline"> HTML5</div>
+                            <div class="badge badge-outline"> CSS3</div>
+                            <div class="badge badge-outline"> MongoDB</div>
+                            <div class="badge badge-outline"> Bootstrap 5</div>
+                        </div>
+                        <p className='flex justify-center items-center gap-5 m-auto'>
+                            <a href="https://coders-shopping.web.app/" rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillEye /></span>&nbsp; Live</a>
+                            <a href='https://github.com/Sohailul/coders-shopping' rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillGithub /></span>&nbsp; GitHub</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://live.staticflickr.com/65535/52093066586_828af4ab1b_b.jpg" alt="" /></figure>
+                    <div class="card-body bg-secondary text-white">
+                        <h2 className="text-3xl font-bold text-center mb-2">GYM WareHouse</h2>
+                        <div class="card-actions">
+                            <div class="badge badge-outline">React</div>
+                            <div class="badge badge-outline"> Node Js</div>
+                            <div class="badge badge-outline"> Express JS</div>
+                            <div class="badge badge-outline"> HTML5</div>
+                            <div class="badge badge-outline"> CSS3</div>
+                            <div class="badge badge-outline"> MongoDB</div>
+                            <div class="badge badge-outline"> Bootstrap 5</div>
+                        </div>
+                        <p className='flex justify-center items-center gap-5 mt-3 m-auto'>
+                            <a href="https://gym-warehouse-management.web.app/" rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillEye /></span>&nbsp; Live</a>
+                            <a href='https://github.com/Sohailul/gym-warehouse-management' rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillGithub /></span>&nbsp; GitHub</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://live.staticflickr.com/65535/52093303399_b3ca9779f8_b.jpg" alt="" /></figure>
+                    <div class="card-body bg-secondary text-white">
+                        <h2 className="text-3xl font-bold text-center mb-2">Product Analysis</h2>
+                        <div class="card-actions">
+                            <div class="badge badge-outline">React</div>
+                            <div class="badge badge-outline">React Recharts</div>
+                            <div class="badge badge-outline"> HTML5</div>
+                            <div class="badge badge-outline"> CSS3</div>
+                            <div class="badge badge-outline"> Tailwind CSS</div>
+                        </div>
+                        <p className='flex justify-center items-center gap-5 mt-3 m-auto'>
+                            <a href="https://product-analysis-lul.netlify.app/" rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillEye /></span>&nbsp; Live</a>
+                            <a href='https://github.com/Sohailul/product-analysis' rel="noreferrer" target="_blank" className='btn btn-sm btn-neutral mt-5'><span className='text-2xl'><AiFillGithub /></span>&nbsp; GitHub</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
