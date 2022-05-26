247ea570f784e973b2c685a1b114434abc5929b7
sohailul <sohailulalam@gmail.com>
2022-05-26 17:27:00 +0600

    Changes on Dashboard and banner url

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {CgProfile} from 'react-icons/cg';
-import {FaUsers} from 'react-icons/fa';
-import {BsCartCheckFill} from 'react-icons/bs';
-import {AiOutlineShop, AiFillShopping} from 'react-icons/ai';
-import {MdOutlineManageAccounts, MdReviews} from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
+import { FaUsers } from 'react-icons/fa';
+import { BsCartCheckFill } from 'react-icons/bs';
+import { AiOutlineShop, AiFillShopping } from 'react-icons/ai';
+import { MdOutlineManageAccounts, MdReviews } from 'react-icons/md';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
@@ -22,25 +22,25 @@ const Dashboard = () => {
                 </div>
                 <div className="drawer-side mr-5">
                     <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 overflow-y-auto w-56 bg-primary text-white rounded text-base-content">
+                    <ul className="menu p-4 overflow-y-auto w-56 bg-primary text-white rounded">
                         {/* <!-- Sidebar content here --> */}
 
 
                         {!admin &&
                             <>
-                                <li><Link to="/dashboard/myorders"><AiFillShopping/>My Orders</Link></li>
-                                <li><Link to="/dashboard/review"><MdReviews/>Add a Review</Link></li>
+                                <li><Link to="/dashboard/myorders"><AiFillShopping />My Orders</Link></li>
+                                <li><Link to="/dashboard/review"><MdReviews />Add a Review</Link></li>
                             </>
                         }
-                        <li><Link to="/dashboard"><CgProfile/>My Profile</Link></li>
+                        <li><Link to="/dashboard"><CgProfile />My Profile</Link></li>
                         {
                             admin &&
 
                             <>
-                                <li><Link to="/dashboard/users"><FaUsers/>Users</Link></li>
-                                <li><Link to="/dashboard/manageorders"><BsCartCheckFill/>Manage All Orders</Link></li>
-                                <li><Link to="/dashboard/addproduct"><AiOutlineShop/>Add a Product</Link></li>
-                                <li><Link to="/dashboard/manageproducts"><MdOutlineManageAccounts/>Manage Products</Link></li>
+                                <li><Link to="/dashboard/users"><FaUsers />Users</Link></li>
+                                <li><Link to="/dashboard/manageorders"><BsCartCheckFill />Manage All Orders</Link></li>
+                                <li><Link to="/dashboard/addproduct"><AiOutlineShop />Add a Product</Link></li>
+                                <li><Link to="/dashboard/manageproducts"><MdOutlineManageAccounts />Manage Products</Link></li>
                             </>
 
                         }
--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="hero min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://i.imgur.com/9ilVH5n.png" className="max-w-sm lg:max-w-2xl" alt='' />
+                <img src="https://live.staticflickr.com/65535/52100389066_6e7d83976f_h.jpg" className="max-w-sm lg:max-w-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold text-primary">Quality Manufacturing!</h1>
                     <p className="py-6 font-bold">When we use the term electrical parts, it is meant to intend the functional pieces of your electrical system</p>
